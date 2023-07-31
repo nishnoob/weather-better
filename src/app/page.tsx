0e9c1dbd3dee5d69f70f2af40d6200681a45a70f
Agnish Mukherjee <agnish15m@gmail.com>
2023-07-31 12:15:57 +0530

    Added basic UI to consume the Weather data. Addedd error handling and loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,126 +1,100 @@
 'use client'
-
 import axios from 'axios';
-import Image from 'next/image'
-import { useEffect } from 'react';
+import React, { useState } from 'react';
 
-export default function Home() {
-  useEffect(() => {
-    getWeatherData();
-  }, []);
-  
-  const getWeatherData = async () => {
-    const resp = await axios.get('http://127.0.0.1:3000/api/weather/mumbai');
-    console.log("first", resp.data)
+interface WeatherData {
+  location: string;
+  weather: string;
+  temperature: number;
+  humidity: number;
+  windSpeed: number;
+}
+
+const App: React.FC = () => {
+  const [place, setPlace] = useState('');
+  const [data, setData] = useState<WeatherData | null>(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setError(false);
+    setPlace(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setData(null);
+    setLoading(true);
+    try {
+      const resp = await axios.get<WeatherData>(`http://127.0.0.1:3000/api/weather/${place}`);
+      setData(resp.data);
+    } catch (error:any) {
+      console.error('Error fetching weather data:', error.message);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 bg-opacity-60 backdrop-blur-md">
+      <div className="bg-white rounded-lg shadow-lg w-96 p-8 backdrop-filter backdrop-blur-sm bg-opacity-70">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="place" className="block text-sm font-medium text-gray-700">
+              Place
+            </label>
+            <input
+              type="text"
+              id="place"
+              name="place"
+              className="mt-1 block w-full text-black border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-400 focus:border-blue-400"
+              placeholder="Enter a place"
+              value={place}
+              onChange={handleInputChange}
             />
-          </a>
-        </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white rounded-md px-4 py-2 transition-colors duration-300 hover:bg-blue-600"
+            >
+              {loading ? 'Loading...' : 'Get Weather'}
+            </button>
+          </div>
+        </form>
+        {/* Display weather data if available */}
+        {data && (
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold">Weather Data:</h3>
+            <p>
+              Location: {data.location}
+              <br />
+              Weather: {data.weather}
+              <br />
+              Temperature: {data.temperature}°C
+              <br />
+              Humidity: {data.humidity}%
+              <br />
+              Wind Speed: {data.windSpeed} m/s
+            </p>
+          </div>
+        )}
+        {/* Display loading state */}
+        {loading && !data && (
+          <div className="mt-4">
+            <p className="text-sm text-gray-500">Loading weather data...</p>
+          </div>
+        )}
+        {/* Display error state */}
+        {!loading && !data && error && (
+          <div className="mt-4">
+            <p className="text-red-500">Error fetching weather data. Please try again.</p>
+          </div>
+        )}
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
